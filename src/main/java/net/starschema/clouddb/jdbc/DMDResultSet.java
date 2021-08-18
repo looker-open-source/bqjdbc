566620ecbcfe9037314174bd96fa3c1ddfc42976
Kalen Petersen <kalenp@google.com>
2021-08-18 23:09:21 +0000

    Remove extraneous semicolons in class definition

--- src/main/java/net/starschema/clouddb/jdbc/DMDResultSet.java
+++ src/main/java/net/starschema/clouddb/jdbc/DMDResultSet.java
@@ -485,7 +485,7 @@ public class DMDResultSet extends ScrollableResultset<Object> implements java.sq
   public void close() throws SQLException {
     super.close();
     this.Colnames = null;
-  };
+  }
 
   /** {@inheritDoc} */
   @Override
@@ -506,7 +506,7 @@ public class DMDResultSet extends ScrollableResultset<Object> implements java.sq
   @Override
   public <T> T getObject(String columnLabel, Class<T> type) throws SQLException {
     throw new BQSQLException("Not implemented.");
-  };
+  }
 
   /**
    *
