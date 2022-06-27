99e15c14ca7351079cdc7f06327336fe0579c0a2
Farid Zakaria <fmzakari@google.com>
2022-06-27 14:29:03 -0700

    Add Google Drive scope for OAuth2 (#140)
    * Add Google Drive scope for OAuth2
    
    Add Google Drive scope to all locations where we create Google
    Credentials.
    
    Changed the Drive scope to be read-only.
    
    * removed scopes for service account method

--- src/main/java/net/starschema/clouddb/jdbc/Oauth2Bigquery.java
+++ src/main/java/net/starschema/clouddb/jdbc/Oauth2Bigquery.java
@@ -86,7 +86,7 @@ public class Oauth2Bigquery {
   /** Application name set on bigquery connection */
   static final String applicationName = "BigQuery JDBC Driver";
 
-  private static final String DRIVE_SCOPE = "https://www.googleapis.com/auth/drive";
+  private static final String DRIVE_SCOPE = "https://www.googleapis.com/auth/drive.readonly";
 
   private static final Integer DEFAULT_IMPERSONATION_LIFETIME = 3600;
 
@@ -174,7 +174,9 @@ public class Oauth2Bigquery {
       List<String> targetServiceAccounts,
       String projectId)
       throws SQLException {
-    GoogleCredentials credential = GoogleCredentials.create(new AccessToken(oauthToken, null));
+    GoogleCredentials credential =
+        GoogleCredentials.create(new AccessToken(oauthToken, null))
+            .createScoped(GenerateScopes(false));
 
     logger.debug("Creating a new bigquery client.");
 
@@ -337,7 +339,8 @@ public class Oauth2Bigquery {
       List<String> targetServiceAccounts,
       String projectId)
       throws IOException {
-    GoogleCredentials credential = GoogleCredentials.getApplicationDefault();
+    GoogleCredentials credential =
+        GoogleCredentials.getApplicationDefault().createScoped(GenerateScopes(false));
 
     logger.debug("Authorizing with Application Default Credentials.");
 
